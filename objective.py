4e3c3e817d1973dd83dbe96439646f3cda47b83f
Badr-MOUFAD <badr.moufad@emines.um6p.ma>
2022-11-23 13:49:35 +0100

    to_dict --> get_objective

--- objective.py
+++ objective.py
@@ -21,7 +21,7 @@ class Objective(BaseObjective):
         diff = self.y - self.X.dot(beta)
         return .5 * diff.dot(diff)
 
-    def to_dict(self):
+    def get_objective(self):
         return dict(X=self.X, y=self.y, fit_intercept=self.fit_intercept)
 
     def get_one_solution(self):
